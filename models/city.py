0a6a5d030c43b778ca39830eadb3dd20dca6ea95
glackycode <glackycode@gmail.com>
2023-08-12 13:54:25 +0000

    adding city

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining City Class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Representing a city
+    
+    Attribs:
+        state_id (str): id of the state
+        name (str): name of the city
+    """
+    state_id = ""
+    name = ""
