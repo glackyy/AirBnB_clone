2d42b583eb5df770322c5ced35c8625b0ce660ef
glackycode <glackycode@gmail.com>
2023-08-12 13:53:48 +0000

    adding user

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defining User Class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Representing a User
+    
+    Attribs:
+        email (str): Email of the user
+        password (str): Password of the user
+        first_name (str): First name of the user
+        last_name (str): Last name of the user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
