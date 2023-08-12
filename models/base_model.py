a640dd467b77287c8b4dc155063e610d58290157
glackycode <glackycode@gmail.com>
2023-08-12 15:39:41 +0000

    pycodestyle + modif

--- models/base_model.py
+++ models/base_model.py
@@ -5,17 +5,22 @@ from models import storage
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Representing Base Model of the hbnb"""
 
     def __init__(self, *args, **kwargs):
         """Initializing new BaseModel
-        
+
         Args:
             *args: Var-length arg list (not using it)
-            **kwargs: Keyword args to recreate an instance from a dic representation. 
+            **kwargs: Keyword args to recreate an instance from a
+            dic representation. 
         """
         timef = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, val in kwargs.items():
                 if k == '__class__':
@@ -24,27 +29,25 @@ class BaseModel:
                     val = datetime.strptime(val, timef)
                 setattr(self, k, val)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
-    
+            models.storage.new(self)
+
     def __str__(self):
         """Returns a string representation of the instance"""
 
         classname = self.__class__.__name__
         return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
-    
+
     def save(self):
         """Update the 'updated_at' attrib with the current datetime"""
-        storage.save()
-        self.updated_at = datetime.now()
-    
+        models.storage.save()
+        self.updated_at = datetime.today()
+
     def to_dict(self):
         """Convert the instance to a dic representation
-        
+
         Returns:
-            dict: A dictionnary containing the attribs and values of the instance.
+            dict: A dictionnary containing the attribs and
+             values of the instance.
         """
         classname = self.__class__.__name__
         object_dict = self.__dict__.copy()
