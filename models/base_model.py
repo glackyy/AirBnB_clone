84c089a0d9890af6d6097b6e3d1125af24c59beb
glackycode <glackycode@gmail.com>
2023-08-13 09:39:52 +0000

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,16 +17,15 @@ class BaseModel:
             dic representation. 
         """
         timef = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, val in kwargs.items():
-                if k == '__class__':
-                    continue
-                if k in ['created_at', 'updated_at']:
-                    val = datetime.strptime(val, timef)
-                setattr(self, k, val)
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, timef)
+                else:
+                    self.__dict__[key] = val
         else:
             models.storage.new(self)
 
