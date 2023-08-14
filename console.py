98d0d9f43eee872fa94519423a8fc55f3d430077
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2023-08-14 11:44:47 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,131 +1,87 @@
 #!/usr/bin/python3
 """ Console module for AirBnB """
 import cmd
-import re
-from shlex import split
 from models.base_model import BaseModel
-from models import storage
+from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
-from models.user import User
-from models.review import Review
-from models.place import Place
 from models.amenity import Amenity
-
-
-def spliter(arg):
-    """Spliter For spliting the arg"""
-
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
+from models.review import Review
+from models import storage
+import re
+import json
 
 
 class HBNBCommand(cmd.Cmd):
     """Defines the command interpreter."""
 
     prompt = "(hbnb) "
-    __classes = {"BaseModel": BaseModel, "User": User, "State": State,
-                 "City": City, "Amenity": Amenity, "Place": Place,
-                 "Review": Review}
+    classes = {"BaseModel", "State", "City",
+               "Amenity", "Place", "Review", "User"}
 
-    def do_EOF(self, sta):
+    def do_EOF(self, line):
         """EOF Ctrl-D end of file to exit"""
         return True
 
-    def do_quit(self, sta):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
     def emptyline(self):
         """repeats line on receiving empty line"""
         pass
    
-    def do_create(self, arg):
-        """Create a new instance of BaseModel, save it and print the id
-
-        Args:
-            arg: contains the name of the class and id to be created
-        """
-        args = spliter(arg)
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.__classes.keys():
+    def do_create(self, line):
+        """Create instance specified by user"""
+        if len(line) == 0:
+            print("** class name required **")
+        elif line not in HBNBCommand.classes:
             print("** class doesn't exist **")
         else:
-            obj = self.__classes[args[0]]()
-            print(obj.id)
-            storage.save()
+            instance = eval(line)()
+            instance.save()
+            print(instance.id)
 
-    def help_create(self):
-        """ Help information for the create method """
-        print("Creates a class of any type")
-        print("[Usage]: create <className>\n")
-
-    def do_show(self, arg):
+    def do_show(self, line):
         """Print the string representation of an instance based
            on the class name and id
-
-        Args:
-            arg: contains the class name and id to be printed
         """
-        args = spliter(arg)
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.__classes.keys():
+       if len(line) == 0:
+            print("** class name required **")
+            return
+        args = parse(line)
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        elif len(args) == 1:
+            return
+        try:
+            if args[1]:
+                name = "{}.{}".format(args[0], args[1])
+                if name not in storage.all().keys():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[name])
+        except IndexError:
             print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key not in storage.all():
-                print("** no instance found **")
-            else:
-                print(storage.all()[key])
 
-    def help_show(self):
-        """ Help information for the show command """
-        print("Shows an individual instance of a class")
-        print("[Usage]: show <className> <objectId>\
-              or <class name>.show(<id>)\n")
-
-    def do_destroy(self, arg):
-        """Delete an instance based on the class name and id
-
-        Args:
-            arg: contains the class name and id to be deleted
-        """
-        args = spliter(arg)
-        if len(args) == 0:
+    def do_destroy(self, line):
+        """Delete an instance based on the class name and id"""
+        if len(line) == 0:
             print("** class name missing **")
-        elif args[0] not in self.__classes.keys():
+            return
+        args = parse(line)
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        elif len(args) == 1:
+            return
+        try:
+            if args[1]:
+                name = "{}.{}".format(args[0], args[1])
+                if name not in storage.all().keys():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[name]
+                    storage.save()
+        except IndexError:
             print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key not in storage.all().keys():
-                print("** no instance found **")
-            else:
-                del storage.all()[key]
-                storage.save()
-
-    def help_destroy(self):
-        """ Help information for the destroy command """
-        print("Destroys an individual instance of a class")
-        print("[Usage]: destroy <className> <objectId> " +
-              "or <class name>.destroy(<id>)\n")
 
     def do_all(self, arg):
         """Print all string representation of all instances
@@ -232,6 +188,31 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the count command """
         print("Count all objects of a class")
         print("[Usage]: count <className> or <className>.count()\n")
+
+
+def spliter(arg):
+    """Spliter For spliting the arg"""
+
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
+def parse(line):
+    """Helper method to parse user typed input"""
+    return tuple(line.split())
 
 
 if __name__ == '__main__':
